cf08b2e8f3f6eee619062ee16970db021b303eaf
逸凡凡 <@Lenovo-PC>
2017-01-15 16:55:45 +0100

    first commit without itp

---
+++ ics/src/main/java/wanxg/ibo/ics/App.java
@@ -0,0 +1,13 @@
+package wanxg.ibo.ics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
